65f632802c9138ca526c978ee4c558503f15e8c8
Alexis <alex10o@hotmail.com>
2022-01-28 19:15:47 -0300

    reactivo creado

--- modulo 1/angular_cli/PrimerApp/src/app/app.module.ts
+++ modulo 1/angular_cli/PrimerApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent} from './home/home.componet';
 import { TablaAlumnosComponent } from './tabla-alumnos/tabla-alumnos.component';
+import { ReactivoComponent } from './reactivo/reactivo.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TablaAlumnosComponent
+    TablaAlumnosComponent,
+    ReactivoComponent
   ],
   imports: [
     BrowserModule
