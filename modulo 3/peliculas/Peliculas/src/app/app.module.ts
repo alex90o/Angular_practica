5887fa0bcf3781b9315e2326964d9aaf7ff8bb06
Alexis <alex10o@hotmail.com>
2022-01-31 13:10:14 -0300

    creando el proyecto pelicula de ejemplo

---
+++ modulo 3/peliculas/Peliculas/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ListadoComponent } from './listado/listado.component';
+import { DetalleComponent } from './detalle/detalle.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListadoComponent,
+    DetalleComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
