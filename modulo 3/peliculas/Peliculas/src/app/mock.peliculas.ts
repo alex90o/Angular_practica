cfe0772129a9a15d04308de6ce91cb23eb000385
Alexis <alex10o@hotmail.com>
2022-01-31 13:51:50 -0300

    creando el monk peliculas

---
+++ modulo 3/peliculas/Peliculas/src/app/mock.peliculas.ts
@@ -0,0 +1,9 @@
+import { Pelicula } from "./entidades/Pelicula";
+
+export const PELICULAS: Pelicula[] = [
+  {id_pelicula: 1, titulo:"Hombres de negro", director:"Barry Sonnenfeld", genero:"ciencia ficción", foto:"https://tupersonajefavorito.com/wp-content/uploads/2018/05/hombres_de_negro_1997_6-1.jpg"},
+  {id_pelicula: 2, titulo:"Madagascar 3: Los fugitivos", director:"Eric Darnell", genero:"Animación",foto:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSndn5Zsq4KWE07AvT3tUJLpfnNp3AkjnZFb3gt6JsKequUv2gZ"},
+  {id_pelicula: 3, titulo:"The Terminator",director:"James Cameron", genero:"ciencia ficción",foto:"https://pics.filmaffinity.com/Terminator-778052251-large.jpg"}
+];
+
+
